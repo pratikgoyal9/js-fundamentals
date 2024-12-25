06ae20d7f3218c7038204a411a2982c210ff08f8
pratikgoyal <pratik.goyal@valueblue.com>
2024-12-25 13:55:22 +0100

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score1 = "33";
+console.log(typeof score1);  // string
+let numberInScore1 = Number(score1);    // convert string to a number
+console.log(numberInScore1); // 33
+console.log(typeof numberInScore1);  // number
+
+let score2 = "33ab";
+let numberInScore2 = Number(score2);
+console.log(numberInScore2); // NaN
+console.log(typeof numberInScore1);  // number
+
+let score3 = null;
+let numberInScore3 = Number(score3);
+console.log(numberInScore3); // 0
+console.log(typeof numberInScore3);  // number
+
+let score4 = undefined;
+let numberInScore4 = Number(score4);
+console.log(numberInScore4); // NaN
+console.log(typeof numberInScore4);  // number
+
+let score5 = true;
+let numberInScore5 = Number(score5);
+console.log(numberInScore5); // 1
+console.log(typeof numberInScore5);  // number
