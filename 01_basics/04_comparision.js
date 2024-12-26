fb3f325c6c2c8b1b053bcd262b37a342602f578e
pratikgoyal <pratik.goyal@valueblue.com>
2024-12-26 10:23:02 +0100

    fix

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,5 +1,5 @@
-console.log("2" < 1);   // true
-console.log("02" < 1);  // true
+console.log("2" < 1);   // false
+console.log("02" < 1);  // false
 
 console.log(null == 0);     // false
 console.log(null <= 0);     // true
