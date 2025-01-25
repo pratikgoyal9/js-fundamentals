f6419bb53f32f5f2c809da7034663dc26aa05ec0
pratikgoyal9 <ipratikgoyal@yahoo.com>
2025-01-25 22:23:51 +0100

    update file name

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,10 +1,9 @@
 console.log("2" < 1);   // false
 console.log("02" < 1);  // false
 
-console.log(null == 0);     // false
 console.log(null <= 0);     // true
 console.log(null >= 0);     // true
-
+console.log(null == 0);     // false
 
 /*
 comparision: < > <= >=
