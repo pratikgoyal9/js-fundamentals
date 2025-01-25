e036a18ac2f7082258e3f85d296b51070f5ebef4
pratikgoyal9 <ipratikgoyal@yahoo.com>
2025-01-25 22:23:52 +0100

    includesFunction

---
+++ 02_basics/10_includesFunction_arrays.js
@@ -0,0 +1,7 @@
+const numbers = [0, 1, 3, 4, 5, 6, 7];
+
+const resultOne = numbers.includes(1);
+console.log(resultOne); // true
+
+const resultTwo = numbers.includes(2);
+console.log(resultTwo); // false
