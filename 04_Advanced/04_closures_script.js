840d04398c2f0d69c15890e2e110f6a3996cb6b0
pratikgoyal9 <ipratikgoyal@yahoo.com>
2025-01-25 22:23:53 +0100

    currying

--- 04_Advanced/04_closures_script.js
+++ 04_Advanced/04_closures_script.js
@@ -30,16 +30,18 @@ function adder(num) {
   return addTo;
 }
 
-const fn2 = adder(6);
-const fn3 = adder(10);
+const addFn = adder(6);
+const addFn2 = adder(10);
 
-fn2(1); // 7
-fn2(2); // 8
-fn2(3); // 9
+addFn(1); // 7
+addFn(2); // 8
+addFn(3); // 9
 
-fn3(1); // 11
-fn3(2); // 12
-fn3(3); // 13
+addFn2(1); // 11
+addFn2(2); // 12
+addFn2(3); // 13
+
+/* -------------------------------------------------------------------------------- */
 
 /* ----------Use case---------------------- */
 
@@ -55,3 +57,77 @@ const countFn = counter();
 
 countFn();
 countFn();
+
+/* -------------------------------------------------------------------------------- */
+
+/* ----------Currying use case
+
+We need to ensure all the parameters i.e. a, b, c are available before doing the sum operation. To ensure this, we can use currying method
+
+Add---------------------- */
+
+function add(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
+}
+
+console.log(add(2)(3)(4)); // 9
+
+console.log(add(2)(3));
+/* ƒ (c) {
+      return a + b + c;
+    } */
+
+console.log(add(2));
+/* ƒ (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  } */
+
+/* -----------Same use case of Currying in modern JS
+
+Add-----------------------------
+*/
+
+const addModernJS = (a) => (b) => (c) => a + b + c;
+console.log(addModernJS(11)(12)(13));
+
+/* -------------------------------------------------------------------------------- */
+
+/* -----------Another use case of Currying
+
+Sending email-----------------------------
+*/
+
+function sendEmail(to) {
+  return function (sub) {
+    return function (body) {
+      console.log(
+        `Sending email: Receipient is ${to}, subject is ${sub} and body is ${body}`,
+      );
+    };
+  };
+}
+
+const user1 = sendEmail("pratikgoyal9@github.com");
+const user2 = user1("Send new order");
+
+user2("Here is your body"); // Sending email: Receipient is pratikgoyal9@github.com, subject is Send new order and body is Here is your body
+
+/* -------------------------------------------------------------------------------- */
+
+/* -----------Same  use case of Currying in modern JS
+
+Sending email-----------------------------
+*/
+
+const sendEmail3 = (to) => (sub) => (body) =>
+  `Sending emails: Receipient is ${to}, subject is ${sub} and body is  ${body}`;
+
+console.log(
+  sendEmail3("pratikgoyal9@github.com")("Send new orders")("Here is your body"),
+); // Sending emails: Receipient is pratikgoyal9@github.com, subject is Send new orders and body is Here is your body
