4ca4535f67fab6e2787ce8ab953069f7886ca3b9
pratikgoyal9 <ipratikgoyal@yahoo.com>
2025-01-25 22:23:52 +0100

    update notes for array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -22,13 +22,35 @@ const newArray1 = myArray.join();
 console.log(newArray1); // [ 1, 2, 'dog', true, 5 ]
 console.log(newArray1); // 1,2,dog,true,5
 
-const newArray2 = myArray.slice(1, 3);
-console.log(myArray);
+/* 
+1. slice() helps to return the part of the array. Syntax: arrayName.slice(2, 5)
+1. Start index would include the start.
+1. End index would include the end - 1. Ex. arrayName.slice(2, 5). It will consider from 2 to 4
+1. When end index is not mentioned, it will consider the last index of the array. Ex. arrayName.slice(2)
+1. When start index is also not mentioned, it wil return the original array. Ex. arrayName.slice()
+1. Does not modify the original array
+*/
+
+const newArray = [1, 2, "dog", true];
+const newArray2 = newArray.slice(1, 3);
+console.log("Original array after slice: ", newArray);
 console.log(newArray2); // [ 2, 'dog' ]
 
-const newArray3 = myArray.splice(1, 3);
-console.log(myArray); // [ 1, 5 ]
-console.log(newArray3); // [ 2, 'dog', true ]
+/* ************************************************************* */
+/* 
+1. splice() helps to remove the part of the array. Syntax: arrayName.splice(2, 5)
+1. It returns the removed part
+1. It modifies the original array by removing elements from it
+1. Start index would include the start.
+1. End index would include the end. Ex. arrayName.splice(2, 5). It will consider from 2 to 5
+1. When end index is not mentioned, it will consider the last index of the array. Ex. arrayName.splice(2)
+1. When start index is also not mentioned, it wil return the original array. Ex. arrayName.splice()
+*/
+
+const anotherArray = [1, 2, 3, 4, 5, 6];
+const splicedArray = anotherArray.splice(1, 3);
+console.log("Original array after splice: ", anotherArray); // [ 1, 5, 6 ]
+console.log(splicedArray); // [ 2, 3, 4 ]
 
 /* Notes:
 1. if array is declared using, it cannot be reassigned 
@@ -36,6 +58,8 @@ console.log(newArray3); // [ 2, 'dog', true ]
 1. Arrays in JS are homoegenous. It can also contain different types of data types
 */
 
-// newArray3 = ["Alexa"]; // will return error
+// splicedArray = ["Alexa"]; // will return error
 
-newArray3[0] = ["Alexa"]; // will run fine
+splicedArray[0] = ["Alexa"]; // will run fine
+
+console.log(splicedArray); // [ 'Alexa', 3, 4 ]
